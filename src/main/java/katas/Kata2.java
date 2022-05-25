b0c481e6b92b6a2c45b95a2fd25534cc20e77940
luis <luisfelorivas@gmai.com>
2022-05-25 08:46:10 -0500

    solve Kata2 and fix Kata2Test error in expected size ( change: 4-> 2).

--- src/main/java/katas/Kata2.java
+++ src/main/java/katas/Kata2.java
@@ -7,6 +7,7 @@ import util.DataUtil;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /*
     Goal: Chain filter() and map() to collect the ids of videos that have a rating of 5.0
@@ -17,6 +18,11 @@ public class Kata2 {
     public static List<Integer> execute() {
         List<Movie> movies = DataUtil.getMovies();
 
-        return ImmutableList.of(1, 2, 3);
+        return movies
+                .stream()
+                .filter(movie -> movie.getRating().equals(5D))
+                .map(movie -> movie.getRating().intValue())
+                .collect(Collectors.toList());
+
     }
 }
