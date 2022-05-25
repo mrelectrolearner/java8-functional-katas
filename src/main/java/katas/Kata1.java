9b85003f39e07cbe360f0e3b1332e6bffb1980d2
luis <luisfelorivas@gmai.com>
2022-05-25 08:34:37 -0500

    solve Kata1

--- src/main/java/katas/Kata1.java
+++ src/main/java/katas/Kata1.java
@@ -1,12 +1,15 @@
 package katas;
 
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
 import model.Movie;
 import util.DataUtil;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /*
     Goal: use map() to project an array of videos into an array of {id, title}-pairs
@@ -16,7 +19,10 @@ import java.util.Map;
 public class Kata1 {
     public static List<Map> execute() {
         List<Movie> movies = DataUtil.getMovies();
+        List<Map> list=movies.stream().map(movie -> {
+            return Map.of("id", movie.getId().toString(), "title",movie.getTitle().toString());
+        }).collect(Collectors.toList());
 
-        return ImmutableList.of(ImmutableMap.of("id", 5, "title", "Bad Boys"));
+        return list;
     }
 }
