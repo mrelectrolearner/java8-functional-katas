d21b283b9027e3cfd505aa20fc5a43e1bf7eadad
luis <luisfelorivas@gmai.com>
2022-05-25 09:14:07 -0500

    solve Kata4

--- src/main/java/katas/Kata4.java
+++ src/main/java/katas/Kata4.java
@@ -6,8 +6,12 @@ import model.BoxArt;
 import model.MovieList;
 import util.DataUtil;
 
+import javax.swing.*;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 /*
     Goal: Retrieve id, title, and a 150x200 box art url for every video
@@ -18,6 +22,16 @@ public class Kata4 {
     public static List<Map> execute() {
         List<MovieList> movieLists = DataUtil.getMovieLists();
 
-        return ImmutableList.of(ImmutableMap.of("id", 5, "title", "Bad Boys", "boxart", new BoxArt(150, 200, "url")));
+        Predicate<BoxArt> correctBoxArt= boxArt -> boxArt.getHeight().equals(150)&&boxArt.getWidth().equals(200);
+
+        return movieLists.stream()
+                .flatMap(movieList->movieList.getVideos().stream())
+                .map(movie -> Map.of("id",movie.getId(),
+                        "title",movie.getTitle(),
+                        "boxart",movie.getBoxarts()
+                                .stream()
+                                .filter(correctBoxArt)))
+                .collect(Collectors.toList());
+
     }
 }
