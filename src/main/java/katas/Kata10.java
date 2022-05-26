6764272a261b8d8035b4dd7e05a989288cecd887
luis <luisfelorivas@gmai.com>
2022-05-25 20:38:02 -0500

    solve Kata10 and fix Kata10Test

--- src/main/java/katas/Kata10.java
+++ src/main/java/katas/Kata10.java
@@ -1,13 +1,11 @@
 package katas;
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
-import model.MovieList;
 import util.DataUtil;
 
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /*
     Goal: Create a datastructure from the given data:
@@ -55,9 +53,21 @@ public class Kata10 {
         List<Map> lists = DataUtil.getLists();
         List<Map> videos = DataUtil.getVideos();
 
-        return ImmutableList.of(ImmutableMap.of("name", "someName", "videos", ImmutableList.of(
-                ImmutableMap.of("id", 5, "title", "The Chamber"),
-                ImmutableMap.of("id", 3, "title", "Fracture")
-        )));
+
+
+        return lists.stream().map(list-> {
+            Stream<Map> videosOfTheList = videos.stream().filter(video -> video.get("listId").equals(list.get("id")));
+            return Map.of("name",
+                    list.get("name"),
+                    "video",
+                    videosOfTheList
+                            .map(video->Map.of("id",video.get("id"),"title",video.get("title")))
+                            .collect(Collectors.toSet()));
+            }).collect(Collectors.toList());
+
+
+
+
+
     }
 }
