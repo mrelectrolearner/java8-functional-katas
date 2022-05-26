2be0a1fb0a01414819f8710bef87d6198b005379
luis <luisfelorivas@gmai.com>
2022-05-25 20:36:52 -0500

    solve Kata 6 and fix Kata6Test

--- src/main/java/katas/Kata6.java
+++ src/main/java/katas/Kata6.java
@@ -1,9 +1,11 @@
 package katas;
 
+import model.BoxArt;
 import model.Movie;
 import util.DataUtil;
 
 import java.util.List;
+import java.util.function.BinaryOperator;
 
 /*
     Goal: Retrieve the url of the largest boxart using map() and reduce()
@@ -13,7 +15,13 @@ import java.util.List;
 public class Kata6 {
     public static String execute() {
         List<Movie> movies = DataUtil.getMovies();
+        BinaryOperator<BoxArt> largestBoxArt=(boxArt1, boxArt2) -> {
+            return boxArt1.getHeight()* boxArt1.getWidth() > boxArt2.getHeight()* boxArt1.getWidth() ? boxArt1 : boxArt2;
+        };
+        return movies.stream()
+                .flatMap(movie -> movie.getBoxarts().stream())
+                .reduce(largestBoxArt)
+                .map(boxArt -> boxArt.getUrl()).orElseThrow();
 
-        return "someUrl";
     }
 }
